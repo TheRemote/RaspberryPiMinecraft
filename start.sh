1262ff5a633f46131290ac02d4b768fb7f575cba
Marc Tönsing <marc.toensing@gmail.com>
2018-05-19 10:53:00 +0200

    Translation fix

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 # Marc Tönsing - V1.0 - 18.05.2018
 # Minecraft Server startup script using screen
 echo "Starte Minecraft Server.  Um  screen -r minecraft."
-echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
+echo "Um den screen in den Hintergrund zu schieben, drücke Ctrl+A und dann Ctrl+D"
 cd /home/pi/minecraft/
 /usr/bin/screen -dmS minecraft /usr/lib/jvm/java-9-openjdk-armhf/bin/java -jar -Xms800M -Xmx800M /home/pi/minecraft/paperclip.jar
