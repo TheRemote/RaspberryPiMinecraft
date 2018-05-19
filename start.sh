1aa691f51c3065048803de0c7125ebbd957381f8
Marc Tönsing <marc.toensing@gmail.com>
2018-05-19 10:51:18 +0200

    V1.1 DE

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Marc Tönsing - V1.0 - 18.05.2018
 # Minecraft Server startup script using screen
-echo "Starting Minecraft server.  To view window type screen -r minecraft."
+echo "Starte Minecraft Server.  Um  screen -r minecraft."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
 cd /home/pi/minecraft/
 /usr/bin/screen -dmS minecraft /usr/lib/jvm/java-9-openjdk-armhf/bin/java -jar -Xms800M -Xmx800M /home/pi/minecraft/paperclip.jar
