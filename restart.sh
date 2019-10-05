7e301e14fbbbd10cee605cef8232071c1317dc46
Benjamin G <therealrandomblock1@gmail.com>
2019-10-05 12:37:20 -0500

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -11,30 +11,30 @@ fi
 echo "Sending restart notifications to server..."
 
 # Minecraft Server restart and pi reboot.
-screen -Rd minecraft -X stuff "say Server is restarting in 30 seconds! $(printf '\r')"
+screen -Rd nukkitx -X stuff "say Server is restarting in 30 seconds! $(printf '\r')"
 sleep 23s
-screen -Rd minecraft -X stuff "say Server is restarting in 7 seconds! $(printf '\r')"
+screen -Rd nukkitx -X stuff "say Server is restarting in 7 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd minecraft -X stuff "say Server is restarting in 6 seconds! $(printf '\r')"
+screen -Rd nukkitx -X stuff "say Server is restarting in 6 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd minecraft -X stuff "say Server is restarting in 5 seconds! $(printf '\r')"
+screen -Rd nukkitx -X stuff "say Server is restarting in 5 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd minecraft -X stuff "say Server is restarting in 4 seconds! $(printf '\r')"
+screen -Rd nukkitx -X stuff "say Server is restarting in 4 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd minecraft -X stuff "say Server is restarting in 3 seconds! $(printf '\r')"
+screen -Rd nukkitx -X stuff "say Server is restarting in 3 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd minecraft -X stuff "say Server is restarting in 2 seconds! $(printf '\r')"
+screen -Rd nukkitx -X stuff "say Server is restarting in 2 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd minecraft -X stuff "say Server is restarting in 1 second! $(printf '\r')"
+screen -Rd nukkitx -X stuff "say Server is restarting in 1 second! $(printf '\r')"
 sleep 1s
-screen -Rd minecraft -X stuff "say Closing server...$(printf '\r')"
-screen -Rd minecraft -X stuff "stop $(printf '\r')"
+screen -Rd nukkitx -X stuff "say Closing server...$(printf '\r')"
+screen -Rd nukkitx -X stuff "stop $(printf '\r')"
 
 # Wait up to 30 seconds for server to close
 echo "Closing server..."
 StopChecks=0
 while [ $StopChecks -lt 30 ]; do
-  if ! screen -list | grep -q "minecraft"; then
+  if ! screen -list | grep -q "nukkitx"; then
     break
   fi
   sleep 1;
