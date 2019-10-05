94332c138cd4adb58119ebca3b0380d97753a00e
Benjamin G <therealrandomblock1@gmail.com>
2019-10-05 12:46:13 -0500

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-# James Chambers - March 2nd 2019
-# Minecraft Server stop script - primarily called by minecraft service but can be ran manually
+# Randomblock1 and James Chambers - October 5th 2019
+# NukkitX Minecraft Server stop script - primarily called by nukkitx service but can be ran manually
 
 # Check if server is running
-if ! screen -list | grep -q "minecraft"; then
+if ! screen -list | grep -q "nukkitx"; then
   echo "Server is not currently running!"
   exit 1
 fi
 
 # Stop the server
-echo "Stopping Minecraft server ..."
-screen -Rd minecraft -X stuff "say Closing server (stop.sh called)...$(printf '\r')"
-screen -Rd minecraft -X stuff "stop$(printf '\r')"
+echo "Stopping NukkitX Minecraft server ..."
+screen -Rd nukkitx -X stuff "say Closing server (stop.sh called)...$(printf '\r')"
+screen -Rd nukkitx -X stuff "stop$(printf '\r')"
 
 # Wait up to 30 seconds for server to close
 StopChecks=0
 while [ $StopChecks -lt 30 ]; do
-  if ! screen -list | grep -q "minecraft"; then
+  if ! screen -list | grep -q "nukkitx"; then
     break
   fi
   sleep 1;
@@ -24,12 +24,12 @@ while [ $StopChecks -lt 30 ]; do
 done
 
 # Force quit if server is still open
-if screen -list | grep -q "minecraft"; then
-  echo "Minecraft server still hasn't closed after 30 seconds, closing screen manually"
-  screen -S minecraft -X quit
+if screen -list | grep -q "nukkitx"; then
+  echo "NukkitX Minecraft server still hasn't closed after 30 seconds, closing screen manually"
+  screen -S nukkitx -X quit
 fi
 
-echo "Minecraft server stopped."
+echo "NukkitX Minecraft server stopped."
 
 # Sync all filesystem changes out of temporary RAM
 sync
