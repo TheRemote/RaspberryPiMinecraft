d5b05db66a3118eed494505d45dd314a71eb0115
FOC <finred@hotmail.co.uk>
2020-09-19 15:15:25 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -91,32 +91,30 @@ sed -i "s:dirname:$DirName:g" start.sh
 sed -i "s:memselect:$MemSelected:g" start.sh
 
 answer="n" 
-    # nested loop for version authentication and user input
+# nested loop for version authentication and user input
+while [[ "$answer" != "y" ]]
+do
     while [[ "$answer" != "y" ]]
     do
-        while [[ "$answer" != "y" ]]
-        do
-            read -p "Enter the version of minecraft you want to run: " mcVer
+        read -p "Enter the version of minecraft you want to run: " mcVer
 
-
-            read -p "Do you want minecraft: $mcVer? [y/n]" answer
-        done
-
-        echo "Getting Paper Minecraft server v$mcVer..."
-
-        # d/l url
-        URL="https://papermc.io/api/v1/paper/$mcVer/latest/download"
-        # will return code 0 if paper version d/l's with no issue 
-        # else will inform user that version entered is invalid and they need to try again
-        wget --spider --quiet "$URL"
-        if [ $? -ne 0 ] 
-        then 
-           
-        else 
-            answer="n"
-        fi
-
+        read -p "Do you want minecraft: $mcVer? [y/n]" answer
     done
+
+    echo "Getting Paper Minecraft server v$mcVer..."
+
+    # d/l url
+    URL="https://papermc.io/api/v1/paper/$mcVer/latest/download"
+    # will return code 0 if paper version d/l's with no issue 
+    # else will inform user that version entered is invalid and they need to try again
+    wget --spider --quiet "$URL"
+    if [ $? -ne 0 ] 
+    then 
+           
+    else 
+        answer="n"
+    fi
+done
 
 # update version in start.sh
 sed -i "s:verselect:$mcVer:g" start.sh
