10bb523972de8b7143f0cca667997a099ca846c2
FOC <finred@hotmail.co.uk>
2020-09-19 13:10:11 +0100

    Comments
    Commenting Get_MC_Version function

--- SetupMinecraft.sh
+++ SetupMinecraft.sh
@@ -193,9 +193,10 @@ Install_Java() {
   fi
 }
 
-##This is the new function to take desired version from the user, check that the paperClip exists and update $Version
+## This is the new function to take desired version from the user, check that the paperClip exists and update $Version
 Get_MC_Version() {
     answer="n" 
+    # nested loop for version authentication and user input
     while [[ "$answer" != "y" ]]
     do
         while [[ "$answer" != "y" ]]
@@ -208,9 +209,10 @@ Get_MC_Version() {
 
         Print_Style "Getting Paper Minecraft server v$mcVer..." "$YELLOW"
 
-
+        # d/l url
         URL="https://papermc.io/api/v1/paper/$mcVer/latest/download"
-
+        # will return code 0 if paper version d/l's with no issue 
+        # else will inform user that version entered is invalid and they need to try again
         wget -q "$URL"
         if [ $? -ne 0 ] 
         then 
