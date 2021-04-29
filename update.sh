86aaffcfa2fdb1c76186ee4352b8c52cdfea8941
simylein <67859356+simylein@users.noreply.github.com>
2021-04-29 23:27:29 +0200

    ask user for server version
    ask user to input server version if version doesn't exist ask for another one

--- update.sh
+++ update.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 # this script updates your server to the latest version available
 
-# defines papermc server version
-Version="1.16.5"
-
 # change to server directory
 cd dirname/minecraft/
 
 # execute stop script
 ./stop.sh
+
+# ask user for desired update version
+echo "To which server version would you like to update? Example: 1.16.5"
+read -p "Your Version: " Version
+VersionExists=false
+while [[ $VersionExists == false ]]; do
+	echo "checking availability of server version $Version ..."
+	wget --spider --quiet https://papermc.io/api/v1/paper/$Version/latest/download
+	if [ "$?" != 0 ]; then
+		VersionExists=false
+		echo "Version $Version does not exists (yet) - please try anotherone"
+		read -p "Your Version: " Version
+	else
+		echo "Version $Version exists! - updating server..."
+		VersionExists=true
+	fi
+done
 
 # test if papermc is avalable and update on success
 wget --spider --quiet https://papermc.io/api/v1/paper/$Version/latest/download
