ac79fb7bfe3a3d74b7219165e8a47c7741f8c8fb
simylein <67859356+simylein@users.noreply.github.com>
2021-05-01 11:34:48 +0200

    added user confirmation prompt
    script now ask user if an update is desired if not it will startup the server again

--- update.sh
+++ update.sh
@@ -7,6 +7,19 @@ cd dirname/minecraft/
 # execute stop script
 ./stop.sh
 
+# ask user for update
+echo "Would you like to update your server to the latest version available?"
+read -p "Your choice [Y/N]: " choice
+regex="^(Y|y|N|n)$"
+while [[ ! ${choice} =~ ${regex} ]]; do
+	read -p "Please press Y or N: " choice
+done
+if ! [[ $choice =~ ^[Yy]$ ]]; then
+	echo "abording update..."
+	./start.sh
+	exit 1
+fi
+
 # grep latest verion from papermc.io
 Version=$(wget -q -O - https://papermc.io/api/v2/projects/paper | rev | cut -d, -f1 | rev)
 Version="${Version:1}"
