3f35cc79e0496f5a0c952d87652af35702a68a2f
zqigolden <zq11110320@gmail.com>
2022-08-02 23:29:09 +0100

    Update SetupMinecraft.sh
    fix a bug when downloading jre.

--- SetupMinecraft.sh
+++ SetupMinecraft.sh
@@ -201,7 +201,7 @@ Install_Java() {
 
   CPUArch=$(uname -m)
   if [[ "$CPUArch" == *"armv7"* || "$CPUArch" == *"armhf"* ]]; then
-    curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4.212 Safari/537.36" https://github.com/adoptium/temurin18-binaries/releases/download/jdk-18.0.2%2B9/OpenJDK18U-jre_x64_linux_hotspot_18.0.2_9.tar.gz -o jre18.tar.gz -L
+    curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4.212 Safari/537.36" https://github.com/adoptium/temurin18-binaries/releases/download/jdk-18.0.2%2B9/OpenJDK18U-jre_arm_linux_hotspot_18.0.2_9.tar.gz -o jre18.tar.gz -L
     tar -xf jre18.tar.gz
     rm -f jre18.tar.gz
     mv jdk-* jre
